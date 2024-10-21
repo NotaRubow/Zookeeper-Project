1d5a6545773b458f1734ec25c54e213c319565ad
rubown_wlhs <rubown@wlhs.wlwv.k12.or.us>
2024-10-21 14:30:34 -0700

    add stuff

--- Zookeeper/src/Zookeeper.java
+++ Zookeeper/src/Zookeeper.java
@@ -1,17 +1,62 @@
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Zookeeper {
     public static void main(String[] args) {
         String animalType = "Camel";
-        String time = "4:37PM";
-        double habitatTemp = 59.66;
-
+        Random temp = new Random();
+        double min = 50.0;
+        double max = 100.0;
+        double randNum = temp.nextDouble();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a");
+        LocalDateTime now = LocalDateTime.now();
+        double showTemp = min + (max - min) * randNum;
         System.out.println("Animal type: " + animalType);
-        System.out.println("Current time: " + time);
-        System.out.println("Habitat temperature: " + habitatTemp+"F");
+        System.out.println("Current time: " + dtf.format(now));
+        System.out.printf("Habitat temperature: %.2fF" ,showTemp);
+        System.out.println();
 
-        String camel = """
+        Scanner habby = new Scanner(System.in);
+        System.out.println("Please enter the number of the habitat you would like to view: \n1. Hippo\n2. Camel\n3. Elephant\n4. Monkey");
+        int hab = habby.nextInt();
+
+        String fileName = "";
+
+        switch (hab) {
+            case 1:
+                fileName = "Zookeeper/animals/hippo.txt";
+                break;
+            case 2:
+                fileName = "Zookeeper/animals/camel.txt";
+                break;
+            case 3:
+                fileName = "Zookeeper/animals/elephant.txt";
+                break;
+            case 4:
+                fileName = "Zookeeper/animals/monkey.txt";
+                break;
+            default:
+                System.out.println("Invalid option. Please select a number between 1 and 4.");
+                habby.close();
+                return;
+        }
+
+        File file = new File(fileName);
+        try (Scanner fr = new Scanner(file)) {
+            while (fr.hasNextLine()) {
+                String i = fr.nextLine();
+                System.out.println(i);
+            }
+        } catch (Exception e) {
+            System.out.println("No file found: " + fileName);
+        }
+
+        habby.close();
+    }
+       /* String camel = """
             Look at our camel
             
              ___.-''''-.
@@ -45,7 +90,6 @@ public class Zookeeper {
         } catch (Exception e){
             System.out.println("No file");
         }
-
+*/
 
     }
-}
